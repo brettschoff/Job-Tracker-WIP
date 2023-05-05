5ef3e18edc0bfcfe4199dbf55885911d66fb8c77
samuel-w-berg <samuel.w.berg@gmail.com>
2023-05-05 15:14:31 -0500

    add loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import { Loader } from 'semantic-ui-react';
+
+export default function LoaderComponent() {
+    return (
+        <Loader size="small" active>
+            Loading...
+        </Loader>
+    );
+    }
