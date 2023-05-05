18b7d593dd2d66dd6325716ece99226ba3219220
brettschoff <brett.schoff@gmail.com>
2023-05-05 12:59:36 -0600

    Adds Signup Page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,1 +1,27 @@
+import { Form, Button } from 'semantic-ui-react'
 
+
+
+export default function SignupPage(){
+    return (
+        <Form color='blue'>
+            <Form.Field>
+                <label>Username:</label>
+                <input placeholder='Username' />
+            </Form.Field>
+            <Form.Field>
+                <label>Email:</label>
+                <input placeholder='Email' />
+            </Form.Field>
+            <Form.Field>
+                <label>Password:</label>
+                <input placeholder='Password' />
+            </Form.Field>
+            <Form.Field>
+                <label>Confirm Password:</label>
+                <input placeholder='Confirm Password' />
+            </Form.Field>
+            <Button type='submit'>Sign Up</Button>
+        </Form>
+    )
+}
